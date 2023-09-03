208ec9ed633907987ef9501b12864bfe13297b52
Goshi <xxxxxxxgt@gmail.com>
2023-09-03 16:31:10 +0900

    Metacomponentsの追加

---
+++ components/Meta.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Meta: React.FC = () => {
+    return (
+        <>
+            <link rel="icon" href="/favicon.ico" />
+            <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+            <meta name="description" content="ミニマムなSNSアプリケーションです" />
+            <meta property="og:title" content="MINI SNS" />
+            <meta property="og:description" content="ミニマムなSNSアプリケーションです" />
+            <meta property="og:url" content="https://firebase-mini-sns.vercel.app/" />
+            <meta property="og:image" content="https://firebase-mini-sns.vercel.app/logo.png" />
+            <meta property="og:site_name" content="MINI SNS" />
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@g_sp54" />
+            <meta name="twitter:url" content="https://firebase-mini-sns.vercel.app/" />
+            <meta name="twitter:title" content="Goshi Tsuboi" />
+            <meta name="twitter:description" content="ミニマムなSNSアプリケーションです" />
+            <meta name="twitter:image" content="https://firebase-mini-sns.vercel.app/logo.png" />
+        </>
+    )
+}
+
+export default Meta
